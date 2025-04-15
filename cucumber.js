7ac593fc7aefa73a07e522e3420da009b4053dc9
Shazad-khan2106 <shazad.khan@techolution.com>
2025-04-15 21:02:48 +0530

    Final commit for the reporting mechanism

--- cucumber.js
+++ cucumber.js
@@ -1,30 +1,39 @@
-import * as os from "node:os";
-import * as process from "node:process";
-import { Status } from "allure-js-commons";
+const os = require("os");
+const process = require("process");
 
 module.exports = {
-    default: {format: ["allure-cucumberjs/reporter"],
-      formatOptions: {
-        resultsDir: "allure-results",
+  default: {
+    require: [
+      'tests/steps/*.ts',
+      'support/*.ts',
+    ],
+    requireModule: ['ts-node/register'],
+    format: [
+      'allure-cucumberjs/reporter',
+      'progress'
+    ],
+    formatOptions: {
+      'allure-cucumberjs/reporter': {
+        resultsDir: './allure-results',
         labels: [
           {
             pattern: [/@epic:(.*)/],
-            name: "epic",
+            name: 'epic',
           },
           {
             pattern: [/@severity:(.*)/],
-            name: "severity",
+            name: 'severity',
           },
         ],
         links: {
           issue: {
             pattern: [/@issue:(.*)/],
-            urlTemplate: "https://issues.example.com/%s",
-            nameTemplate: "ISSUE %s",
+            urlTemplate: 'https://issues.example.com/%s',
+            nameTemplate: 'ISSUE %s',
           },
           tms: {
             pattern: [/@tms:(.*)/],
-            urlTemplate: "https://tms.example.com/%s",
+            urlTemplate: 'https://tms.example.com/%s',
           },
           jira: {
             pattern: [/@jira:(.*)/],
@@ -33,26 +42,18 @@ module.exports = {
         },
         categories: [
           {
-            name: "foo",
-            messageRegex: "bar",
-            traceRegex: "baz",
-            matchedStatuses: [Status.FAILED, Status.BROKEN],
+            name: 'Assertion Errors',
+            messageRegex: '.*AssertionError.*',
+            matchedStatuses: ['failed'],
           },
         ],
         environmentInfo: {
           os_platform: os.platform(),
           os_release: os.release(),
-          os_version: os.version(),
           node_version: process.version,
         },
-      },
-      require: [
-        'tests/steps/*.ts',
-        'support/*.ts'
-      ],
-      requireModule: ['ts-node/register'],
-      format: ['allure-cucumberjs/reporter', 'progress'],
-      paths: ['tests/features/*.feature'],
+      }
     },
-  };
-  
+    paths: ['tests/features/*.feature'],
+  }
+};
