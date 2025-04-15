7ee57df579aac1a2420c4d2c3a20a413faf38585
Shazad-khan2106 <shazad.khan@techolution.com>
2025-04-15 20:56:04 +0530

    Final commit for the reporting mechanism

--- cucumber.js
+++ cucumber.js
@@ -1,5 +1,51 @@
+import * as os from "node:os";
+import * as process from "node:process";
+import { Status } from "allure-js-commons";
+
 module.exports = {
-    default: {
+    default: {format: ["allure-cucumberjs/reporter"],
+      formatOptions: {
+        resultsDir: "allure-results",
+        labels: [
+          {
+            pattern: [/@epic:(.*)/],
+            name: "epic",
+          },
+          {
+            pattern: [/@severity:(.*)/],
+            name: "severity",
+          },
+        ],
+        links: {
+          issue: {
+            pattern: [/@issue:(.*)/],
+            urlTemplate: "https://issues.example.com/%s",
+            nameTemplate: "ISSUE %s",
+          },
+          tms: {
+            pattern: [/@tms:(.*)/],
+            urlTemplate: "https://tms.example.com/%s",
+          },
+          jira: {
+            pattern: [/@jira:(.*)/],
+            urlTemplate: (v) => `https://jira.example.com/browse/${v}`,
+          },
+        },
+        categories: [
+          {
+            name: "foo",
+            messageRegex: "bar",
+            traceRegex: "baz",
+            matchedStatuses: [Status.FAILED, Status.BROKEN],
+          },
+        ],
+        environmentInfo: {
+          os_platform: os.platform(),
+          os_release: os.release(),
+          os_version: os.version(),
+          node_version: process.version,
+        },
+      },
       require: [
         'tests/steps/*.ts',
         'support/*.ts'
