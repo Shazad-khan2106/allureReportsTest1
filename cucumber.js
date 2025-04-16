9a86441e739169e9c4007db3eaf87e38cf3abfc3
Shazad-khan2106 <shazad.khan@techolution.com>
2025-04-16 13:12:50 +0530

    switched to cucumber reporting

--- cucumber.js
+++ cucumber.js
@@ -1,59 +1,12 @@
-const os = require("os");
-const process = require("process");
-
 module.exports = {
-  default: {
-    require: [
-      'tests/steps/*.ts',
-      'support/*.ts',
-    ],
-    requireModule: ['ts-node/register'],
-    format: [
-      'allure-cucumberjs/reporter',
-      'progress'
-    ],
-    formatOptions: {
-      'allure-cucumberjs/reporter': {
-        resultsDir: './allure-results',
-        labels: [
-          {
-            pattern: [/@epic:(.*)/],
-            name: 'epic',
-          },
-          {
-            pattern: [/@severity:(.*)/],
-            name: 'severity',
-          },
-        ],
-        links: {
-          issue: {
-            pattern: [/@issue:(.*)/],
-            urlTemplate: 'https://issues.example.com/%s',
-            nameTemplate: 'ISSUE %s',
-          },
-          tms: {
-            pattern: [/@tms:(.*)/],
-            urlTemplate: 'https://tms.example.com/%s',
-          },
-          jira: {
-            pattern: [/@jira:(.*)/],
-            urlTemplate: (v) => `https://jira.example.com/browse/${v}`,
-          },
-        },
-        categories: [
-          {
-            name: 'Assertion Errors',
-            messageRegex: '.*AssertionError.*',
-            matchedStatuses: ['failed'],
-          },
-        ],
-        environmentInfo: {
-          os_platform: os.platform(),
-          os_release: os.release(),
-          node_version: process.version,
-        },
-      }
+    default: {
+      require: [
+        'tests/steps/*.ts',
+        'support/*.ts'
+      ],
+      requireModule: ['ts-node/register'],
+      format: ['progress'],
+      paths: ['tests/features/*.feature'],
     },
-    paths: ['tests/features/*.feature'],
-  }
-};
+  };
+  
